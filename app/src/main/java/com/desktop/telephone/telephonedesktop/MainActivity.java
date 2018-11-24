ee1ef48109126bd8584c355050b247607897ebfb
huang <huangchongde049@sina.com>
2018-11-25 00:14:50 +0800

    提交项目

--- app/src/main/java/com/desktop/telephone/telephonedesktop/MainActivity.java
+++ app/src/main/java/com/desktop/telephone/telephonedesktop/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initView();
+        
     }
 
     private void initView() {
