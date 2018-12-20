5598c8b569b358b72ecf6cee04b753084c24daaf
huangchongde <huangchongde049@sina.com>
2018-12-20 17:59:33 +0800

    交换机设置界面

---
+++ app/src/main/java/com/desktop/telephone/telephonedesktop/desktop/Activity/TelephoneSettingActivity.java
@@ -0,0 +1,15 @@
+package com.desktop.telephone.telephonedesktop.desktop.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.desktop.telephone.telephonedesktop.R;
+import com.desktop.telephone.telephonedesktop.base.BaseActivity;
+
+public class TelephoneSettingActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telephone_setting);
+    }
+}
